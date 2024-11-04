c80c1ecd048518c766fbcf14115e7e21aff03ffd
ibarradenia23 <yanirisdeniaibarraortiz@gmail.com>
2024-11-04 10:25:58 -0600

    feat: se agrego tailwing.css y su configuracion inicial

---
+++ react-ts/tailwind.config.js
@@ -0,0 +1,11 @@
+import { Flowbite } from 'flowbite-react';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", Flowbite.content()],
+  theme: {
+    extend: {},
+  },
+  plugins: [Flowbite.plugins()],
+}
+
