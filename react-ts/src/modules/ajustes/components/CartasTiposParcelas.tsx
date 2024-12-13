ab7274359dd267a4685e0d7fc0cc5e6f98d99647
ibarradenia23 <yanirisdeniaibarraortiz@gmail.com>
2024-12-12 19:07:16 -0600

    Agregando funcionalidades visuales a tipos de parcela

--- react-ts/src/modules/ajustes/components/CartasTiposParcelas.tsx
+++ react-ts/src/modules/ajustes/components/CartasTiposParcelas.tsx
@@ -1,8 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Modal from '../../../common/components/Modal';
+import { CirclePlus, Map, Pencil, Trash2 } from 'lucide-react';
+import Toast from '../../../common/components/Toast';
+import { eliminarTiposParcelas } from '../api/tiposParcelasService';
+import TiposParcelasForm from './TiposParcelasForm';
 
 const CartasTiposParcelas = () => {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpenEdit, setIsModalOpenEdit] = useState(false);
+    const [toast, setToast] = useState<{
+      type: "success" | "error" | "warning";
+      message: string;
+      visible: boolean;
+    }>({
+      type: "success", // Valor por defecto
+      message: "",
+      visible: false,
+    });
+
+    const closeToast = () => {
+      setToast({ ...toast, visible: false });
+    };
+
+    const handleOpenModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
+    const handleOpenModalEdit = () => {
+      setIsModalOpenEdit(true);
+    };
+  
+    const handleCloseModalEdit = () => {
+      setIsModalOpenEdit(false);
+    };
+
+    const handleEliminar = (id:number) => {
+      if(window.confirm("¿Estas seguro de eliminar este tipo de cultivo?")){
+        eliminarTiposParcelas(id);
+      }
+    }
+
+    const parcelaPrueba = {
+      descripcion:"Parcela top"
+    }
+
+    useEffect(() => {
+        if (toast.visible) {
+          const timer = setTimeout(() => {
+            closeToast();
+          }, 3000); // Duración del toast en milisegundos (3 segundos)
+    
+          return () => clearTimeout(timer); // Limpiar el temporizador al desmontar
+        }
+      }, [toast.visible]);
+  
   return (
-    <div>CartasTiposParcelas</div>
+    <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-h-128">
+    {toast.visible && <Toast type={toast.type} message={toast.message} onClose={closeToast}/>}
+    <div className="flex items-center justify-between">
+      <h3 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white flex items-center gap-2">
+        <Map /> Parcelas
+      </h3>
+      <button className="text-white inline-flex items-center bg-primary hover:bg-[#016F35] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-[#016F35] dark:focus:ring-primary justify-center mt-4 gap-2" onClick={handleOpenModal}>
+        <CirclePlus /> Agregar parcela
+      </button>
+    </div>
+    <div className="contenedorcartas mt-4">
+      <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-between items-center">
+        <div className="">
+          <h4 className="text-[1.2rem] font-bold text-gray-900 dark:text-white">
+            Parcela top
+          </h4>
+        </div>
+        <div className="btones flex gap-2">
+          <button className=" items-center  font-medium rounded-lg text-sm p-2.5 text-center w-full justify-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={handleOpenModalEdit}>
+            <Pencil className="h-4 w-4" />
+          </button>
+
+          <button onClick={()=>handleEliminar(1)} className=" items-center  font-medium rounded-lg text-sm p-2.5 text-center w-full justify-center text-white bg-error hover:bg-red-900 focus:ring-4 focus:ring-gray-100   dark:bg-error dark:hover:bg-red-900 dark:focus:ring-error">
+            <Trash2 className="h-4 w-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+    <Modal isOpen={isModalOpen} title="Crea un tipo de cultivo" onClose={handleCloseModal}>
+      <TiposParcelasForm />
+    </Modal>
+    <Modal isOpen={isModalOpenEdit} title="Edita un tipo de cultivo" onClose={handleCloseModalEdit}>
+      <TiposParcelasForm tipoParcela={parcelaPrueba}/>
+    </Modal>
+  </div>
   )
 }
 
---
+++ react-ts/src/modules/ajustes/components/TiposParcelasForm.tsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import { TiposParcelaInterface } from "../models";
+import { useForm } from "react-hook-form";
+import { useActualizarTiposParcela, useCrearTiposParcela } from "../hooks/useTiposParcelas";
+import Toast from "../../../common/components/Toast";
+import { Pencil } from "lucide-react";
+
+interface TiposParcelaPropsInterface {
+  tipoParcela?: TiposParcelaInterface;
+}
+
+const TiposParcelasForm: React.FC<TiposParcelaPropsInterface> = ({
+  tipoParcela
+}) => {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TiposParcelaInterface>();
+
+   // Usamos el hook para crear un usuario
+    const {
+      mutate: crearTipoParcela,
+      isError: isErrorCrear,
+      isSuccess: isSuccessCrear,
+      error: errorCrear,
+      reset: resetCrear,
+    } = useCrearTiposParcela();
+    const {
+      mutate: editarTipoParcela,
+      isError: isErrorEditar,
+      isSuccess: isSuccessEditar,
+      error: errorEditar,
+      reset: resetEditar,
+    } = useActualizarTiposParcela();
+
+    //Estado para decidir si crear o editar
+      const [isEditing, setIsEditing] = useState(false);
+
+      //Estado para manejar el toast
+         const [toast, setToast] = useState<{
+          type: "success" | "error" | "warning";
+          message: string;
+          visible: boolean;
+        }>({
+          type: "success", // Valor por defecto
+          message: "",
+          visible: false,
+        });
+
+        const closeToast = () => {
+            setToast({ ...toast, visible: false });
+          };
+
+          useEffect(() => {
+              if (tipoParcela) {
+                setIsEditing(true);
+                setValue("descripcion", tipoParcela.descripcion);
+                resetEditar();
+              } else {
+                setIsEditing(false);
+                resetCrear(); // Resetea el estado de creación
+              }
+            }, [tipoParcela, setValue, resetCrear]);
+
+            //Manejador de envio del formulario
+            const onSubmit =async(data:TiposParcelaInterface) =>{
+                if(isEditing && tipoParcela && tipoParcela.id){
+                    editarTipoParcela({id:tipoParcela.id,...data})
+                } else {
+                    crearTipoParcela(data);
+                }
+            }
+
+             useEffect(() => {
+                if (isSuccessCrear) {
+                  setToast({
+                    type: "success",
+                    message: "Tipo de parcelas creado exitosamente.",
+                    visible: true,
+                  });
+                }
+            
+                if (isErrorCrear) {
+                  setToast({
+                    type: "error",
+                    message:
+                      "Error al crear tipo de parcela: " + (errorCrear as Error).message,
+                    visible: true,
+                  });
+                }
+            
+                if (isSuccessEditar) {
+                  setToast({
+                    type: "warning",
+                    message: "Tipo de parcela editado exitosamente.",
+                    visible: true,
+                  });
+                }
+            
+                if (isErrorEditar) {
+                  setToast({
+                    type: "error",
+                    message:
+                      "Error al editar el tipo de parcela: " + (errorEditar as Error).message,
+                    visible: true,
+                  });
+                }
+              }, [isSuccessCrear, isErrorCrear, isSuccessEditar, isErrorEditar]);
+
+             useEffect(() => {
+                  if (toast.visible) {
+                    const timer = setTimeout(() => {
+                      closeToast();
+                    }, 3000); // Duración del toast en milisegundos (3 segundos)
+              
+                    return () => clearTimeout(timer); // Limpiar el temporizador al desmontar
+                  }
+                }, [toast.visible]);
+
+  return (
+    <section>
+              {toast.visible && (
+        <Toast type={toast.type} message={toast.message} onClose={closeToast} />
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-6 space-y-6">
+      <div className="">
+            <label
+              htmlFor="descripcion"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Tipo de cultivo
+            </label>
+            <input
+              type="text"
+              id="cultivo"
+              {...register("descripcion", {
+                required: "Este campo es obligatorio",
+                minLength: {
+                  value: 6,
+                  message: "El tipo de cultivo debe tener al menos 6 caracteres",
+                },
+              })}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-[#016F35] block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Cultivo n"
+            />
+            {errors.descripcion && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.descripcion.message}
+              </p>
+            )}
+          </div>
+          <button
+          type="submit"
+          className={`text-white inline-flex items-center ${
+            isEditing
+              ? "bg-secondary hover:bg-[#8C541D] dark:bg-secondary dark:hover:bg-[#8C541D] dark:focus:ring-secondary"
+              : " bg-primary hover:bg-[#016F35] dark:bg-primary dark:hover:bg-[#016F35] dark:focus:ring-primary"
+          } focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center `}
+        >
+          {isEditing ? (
+            <Pencil className="h-4 w-4 mr-2" />
+          ) : (
+            <svg
+              className="me-1 -ms-1 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          )}
+
+          {isEditing ? "Editar tipo parcela" : "Agregar tipo parcela"}
+        </button>
+      </div>
+      </form>
+    </section>
+  );
+};
+
+export default TiposParcelasForm;
